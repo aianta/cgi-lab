a979e4eec4e474936fb290fb36ec28840ae0614c
Alex <ianta@ualberta.ca>
2022-09-26 18:25:09 -0600

    checking empty string cookies

--- login.py
+++ login.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+
 import cgi
 import os
 from templates import login_page
 from templates import secret_page
 
 def parse_cookies(cookie_string):
-    cookies = cookie_string.split(";")
     result = {}
+    if cookie_string == "":
+        return result
+        
+    cookies = cookie_string.split(";")
     for cookie in cookies:
         split_cookie = cookie.split("=")
         result[split_cookie[0]] = split_cookie[1]
@@ -20,16 +24,13 @@ form = cgi.FieldStorage()
 username = form.getfirst("username")
 password = form.getfirst("password")
 
-
 header = ""
 header += "Content-Type: text/html\r\n"    # HTML is following
 
-
 body = ""
 
-
-
 if username is not None or ('logged' in cookies and cookies['logged'] == "true"):
+# if username is not None:
     body += secret_page(username, password)
     header += "Set-Cookie: logged=true; Max-Age=60\r\n"
     header += "Set-Cookie: cookie=nom\r\n"
